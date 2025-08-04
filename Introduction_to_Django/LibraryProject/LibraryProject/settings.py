07cbbfd856e8fc6069ed5525c4aa4ef4e1483a2b
sbr <babaladesamuel1@gmail.com>
2025-08-04 01:04:28 +0100

    Created bookshelf app with Book model and CRUD operation documentation

--- Introduction_to_Django/LibraryProject/LibraryProject/settings.py
+++ Introduction_to_Django/LibraryProject/LibraryProject/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles', 
+    'bookshelf',  
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
