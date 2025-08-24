a82566062d098efa4ffb461ace993a316d80541f
sbr <babaladesamuel1@gmail.com>
2025-08-24 15:08:24 +0100

    yooo

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -71,8 +71,12 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blogdb',
+        'USER': 'postgres',
+        'PASSWORD': 'SAM20034@',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
